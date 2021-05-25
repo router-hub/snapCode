b1b3d5fd37c5f216b056eca7471ddd2b9fa5b84e
GUPTARAHULONE <65298880+GUPTARAHULONE@users.noreply.github.com>
2021-05-25 23:30:16 +0530

    Create 4.cpp

---
+++ _submissions/18UEC079_RAHUL/Day1/4.cpp
@@ -0,0 +1,12 @@
+int Solution::isPalindrome(string A) {
+     int i = 0, j = A.length()-1;
+    while (i<j)
+    {
+        while(i<j && !isalnum(A[i])) i++;
+        while(i<j && !isalnum(A[j])) j--;
+        if(tolower(A[i])!=tolower(A[j]))
+            return 0;
+        i++; j--;
+    }
+    return 1;
+}
