8314b4e8eb86b89fdb005c7d4381a13a46c4a8db
WinterSoldier13 <ayushsingh1315@gmail.com>
2021-05-27 16:58:55 +0530

    question8

---
+++ _submissions/18UEE071_Ayush/Day5/MAXSPPROD.py
@@ -0,0 +1,16 @@
+class Solution:
+    # @param A : list of integers
+    # @return an integer
+    def maxSpecialProduct(self, arr):
+        s=[]
+        ans = 0
+        for i in range(len(arr)):
+
+            while(len(s)>0 and arr[s[-1]] <= arr[i]):
+                s.pop()
+                if(len(s) > 0):
+                    ans = max(ans, s[-1]*i)
+            s.append(i)
+        if(ans < 0):
+            return 0
+        return ans%1000000007
