07043e894913f8e4034ed78f407c4241c9339444
GUPTARAHULONE <65298880+GUPTARAHULONE@users.noreply.github.com>
2021-05-29 00:42:20 +0530

    Create 13.cpp

---
+++ _submissions/18UEC079_RAHUL/Day3/13.cpp
@@ -0,0 +1,20 @@
+int Solution::minDistance(string A, string B) {
+     int m=A.length(),n=B.length();
+    int dp[m+1][n+1];
+    for(int i=0;i<=m;i++)
+    dp[i][0]=i;
+    for(int j=0;j<=n;j++)
+    dp[0][j]=j;
+    
+    for(int i=1;i<=m;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            if(A[i-1]==B[j-1])
+            dp[i][j]=dp[i-1][j-1];
+            else
+            dp[i][j]=1+min(dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1]));
+        }
+    }
+    return dp[m][n];
+}
