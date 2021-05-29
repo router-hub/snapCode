e6371890fb615a28c6a490445074504dd3cebe78
Chandrika Acharjee <76817938+Chandrika372@users.noreply.github.com>
2021-05-30 04:35:49 +0530

    Edit Distance

---
+++ _submissions/19UCS023_Chandrika/13.cpp
@@ -0,0 +1,20 @@
+int Solution::minDistance(string A, string B) {
+    int m = A.size();
+int n = B.size();
+int dp[m+1][n+1];
+for (int i=0; i<m+1; i++) 
+dp[i][0]=i;
+for (int i=0; i<n+1; i++)
+dp[0][i]=i;
+for (int i=1; i<m+1; i++){
+for (int j=1; j<n+1; j++){
+if (A[i-1]==B[j-1]) //last character of both strings same
+dp[i][j] = dp[i-1][j-1];
+else 
+dp[i][j] =dp[i-1][j-1]+1;
+dp[i][j]=min(dp[i][j],dp[i-1][j]+1);
+dp[i][j]=min(dp[i][j],dp[i][j-1]+1);
+}
+}
+return dp[m][n];
+}
