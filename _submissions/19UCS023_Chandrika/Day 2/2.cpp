e6aa9815a829f2947107086c02a9b8888a8b84f4
Chandrika Acharjee <76817938+Chandrika372@users.noreply.github.com>
2021-05-29 14:52:43 +0530

    Day 2 submission

--- _submissions/19UCS023_Chandrika/Day 2/2.cpp
+++ _submissions/19UCS023_Chandrika/Day 2/2.cpp
@@ -1,18 +1,17 @@
 int Solution::hammingDistance(const vector<int> &A) {
-   long long int sum=0;
-   long long int count;
-   long long int n=A.size();
-   for(int i=0;i<32;i++)
-   {
-       count=0;
-       for(int j=0;j<n;j++)
-       {
-           if(A[j]&(1<<i))
-           count++;
-       }
-   
-   sum=(sum+count*(n-count)*2)%1000000007;
-   }
-   return sum;
-    
-}
+    long long int sum = 0;
+    long long int n=A.size();
+    for (int i = 0; i < 32; i++)
+    {
+      long long int zero_count = 0, one_count = 0;
+      for (int j=0;j<n;j++)
+      {
+        if (A[j] & (1 << i))
+          one_count++;
+        else
+          zero_count++;
+      }
+      sum=(sum+(one_count*zero_count)*2)%1000000007;
+    } 
+    return sum;
+  }
