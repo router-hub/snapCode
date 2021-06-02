7a2dd49e5bd7f7ac31269674010d200c277bc1aa
WinterSoldier13 <ayushsingh1315@gmail.com>
2021-06-02 22:52:02 +0530

    question9

---
+++ _submissions/18UEE071_Ayush/Day6/diffk2.py
@@ -0,0 +1,16 @@
+class Solution:
+    # @param A : list of integers
+    # @param B : integer
+    # @return an integer
+    def diffPossible(self, arr, k):
+        hashset = set()   
+        l = len(arr)
+        if(l==0): return 0
+
+        for x in arr:
+            req = (x-k)
+            req2 = k+x
+            if(req in hashset or req2 in hashset):
+                return 1
+            hashset.add(x)
+        return 0
