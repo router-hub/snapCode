0130d18c1e2c402adcfb5c57455e07f844fe585b
Tharun0102 <rallapallitharun14@gmail.com>
2021-06-03 11:57:46 +0530

    Longest Common subsequence

---
+++ _submissions/19UCS159_Tharun/Day1/LCS.java
@@ -0,0 +1,17 @@
+public class LCS {
+  public int solve(String A, String B) {
+    int n = A.length(), m = B.length();
+    int[][] longest = new int[n + 1][m + 1];
+    // dp[i][j]=(A[i]==B[j])?(1+dp[i-1][j-1])
+    // = dp[i-1][j]
+    // = dp[i][j-1]
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= m; j++) {
+        int op1 = longest[i][j - 1], op2 = longest[i - 1][j];
+        int op3 = (A.charAt(i - 1) == B.charAt(j - 1)) ? (1 + longest[i - 1][j - 1]) : 0;
+        longest[i][j] = Math.max(op1, Math.max(op2, op3));
+      }
+    }
+    return longest[n][m];
+  }
+}
