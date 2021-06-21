1aa1b8104260ebfa4b51dda037f4c661ea1f7f16
Prashun Jha <54842624+prashun07@users.noreply.github.com>
2021-06-21 21:28:55 +0530

    Day10

---
+++ _submissions/18UEI056_Prashun/Day10/Remove_Duplicate.cpp
@@ -0,0 +1,13 @@
+ListNode* Solution::deleteDuplicates(ListNode* A) {
+auto curr = A;
+while(curr->next !=NULL){
+if(curr->val == curr->next->val){
+auto temp = curr->next;
+curr->next = curr->next->next;
+delete temp;
+}else{
+curr = curr->next;
+}
+}
+return A;
+}
